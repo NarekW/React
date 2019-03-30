76f52723a1520b9dc16c1d4ccc90ce5da9a4e0f2
NarekW <vip.hasratyan@mail.ru>
2019-03-30 18:43:36 +0300

    30.03.2019 13:00-18:00

---
+++ src/Moduls/HomepageTwo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/Homepage2.css';
+
+
+class SelTwo extends React.Component {
+    render() {
+      return(
+        <section>
+        <h3>Dare to Grow with AdRoll</h3>
+        </section> 
+      );
+    }
+  }
+  export default SelTwo;
